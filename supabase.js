aa39427295d4c716128089d9921ad761e6965491
wpnwarin-png <wpnwarin@gmail.com>
2026-06-27 10:35:05 +0700

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,10 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = 'https://fzutuwlsbayatkcmjbmo.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRpZHJhbnJneGNnb2dpcnB1dGJ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI1MjE3NTMsImV4cCI6MjA5ODA5Nzc1M30.rVbiV-gzqs2cK9LtGwgKduT2iExmavz3rHIlK6bYZO4'
 
-export const supabase = supabaseUrl && supabaseKey
-  ? createClient(supabaseUrl, supabaseKey)
-  : null
-
-export const isSupabaseReady = !!supabase
+export const supabase = supabaseKey ? createClient(supabaseUrl, supabaseKey) : null
+export const isSupabaseReady = !!supabaseKey
