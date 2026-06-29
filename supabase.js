a3702790e97ea6271af8274df3140aba1f8444ac
wpnwarin-png <wpnwarin@gmail.com>
2026-06-29 13:37:38 +0700

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://fzutuwlsbayatkcmjbmo.supabase.co'
+const supabaseUrl = 'https://tidranrgxcgogirputbz.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRpZHJhbnJneGNnb2dpcnB1dGJ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI1MjE3NTMsImV4cCI6MjA5ODA5Nzc1M30.rVbiV-gzqs2cK9LtGwgKduT2iExmavz3rHIlK6bYZO4'
-
 export const supabase = supabaseKey ? createClient(supabaseUrl, supabaseKey) : null
 export const isSupabaseReady = !!supabaseKey
